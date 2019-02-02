269c957aae105bcb384a5d3971fd8338804e17df
Ayame <46459682+LynConnected@users.noreply.github.com>
2019-02-02 17:14:31 -0500

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 pointA = new Vector3(30, 0, 6);
+    Vector3 pointB = new Vector3(18, 0, 6);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+    }
+}
--- PlayerController.cs
+++ PlayerController.cs
@@ -8,15 +8,23 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public Text allText;
+    public Text livesText;
 
     private Rigidbody rb;
     private int count;
+    private int score;
+    private int lives;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetCountText();
+        score = 0;
+        SetAllText();
+        lives = 3;
+        SetLivesText();
         winText.text = "";
     }
 
@@ -41,14 +49,51 @@ public class PlayerController : MonoBehaviour {
         {
             other.gameObject.SetActive (false);
             count = count + 1;
-            SetCountText(); 
+            SetCountText();
+            score = score + 1; // I added this code to track the score and count separately.
+            SetAllText();
 
+        }
+        if (other.gameObject.CompareTag("Enemy Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+            lives = lives - 1; // this removes 1 from the lives
+            SetLivesText();
+        }
+        if (lives == 0) //this number should be equal to original value lives - lives
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (count == 12) //this number should be equal to the number of yellow pickups on the first playfield
+        {
+            transform.position = new Vector3(23.5f, transform.position.y, 8.0f);
+        }
+        
+    }
+    
+    void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            winText.text = "You Lose!";
         }
     }
     void SetCountText ()
     {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        if (count >= 23)
+        {
+            winText.text = "You Lose!";
+        }
+    }
+    void SetAllText ()
+    {
+        allText.text = "Score: " + score.ToString();
+        if (score >= 20)
         {
             winText.text = "You Win!";
         }
